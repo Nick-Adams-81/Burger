a706d16d24078dc54a31ce10c3410b37ed956d39
nick adams <thebigdeal916@icloud.com>
2020-03-29 17:27:06 -0700

    v1

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers(id, burger_name, devoured)
+VALUES (1, "baco blaster", true);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "still mooing", false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "ham heart attack", true);
+
+SELECT * FROM burgers
+
